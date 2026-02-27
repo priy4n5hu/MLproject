c94a6dd847082783b720d2d9848e07e91ba4f31d
Priyanshu <priyanshujassal68@gmail.com>
2026-02-27 19:46:27 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() #return three important info, last one is imporant on which file and line no the exception has occured
